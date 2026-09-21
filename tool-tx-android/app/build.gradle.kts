2d0ec4ef715c411097c3d20c8036e2b3809f4ffb
tri <ltri15591@gmail.com>
2026-09-21 21:36:37 +0700

    Fix: remove armeabi-v7a ABI (Python 3.12 not available)

--- tool-tx-android/app/build.gradle.kts
+++ tool-tx-android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         versionName = "2.0.0"
 
         ndk {
-            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
+            abiFilters += listOf("arm64-v8a", "x86_64")
         }
     }
 
