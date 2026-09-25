9fe98e102cba5130c5209e63e27034aff3cdad53
tri <ltri15591@gmail.com>
2026-09-25 23:55:46 +0700

    chore: bump version to 2.0.4

--- tool-tx-android/app/build.gradle.kts
+++ tool-tx-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lmt.tooltx"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.3"
+        versionCode = 4
+        versionName = "2.0.4"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
